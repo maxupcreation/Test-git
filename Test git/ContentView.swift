0bbcb407405ee057364fa963bcaad7345fb79df3
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-09-03 19:27:30 +0800

    Initial Commit

---
+++ Test git/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Test git
+//
+//  Created by Giovanni Gaffé on 2021/9/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Test git/Test_gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_gitApp.swift
+//  Test git
+//
+//  Created by Giovanni Gaffé on 2021/9/3.
+//
+
+import SwiftUI
+
+@main
+struct Test_gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
